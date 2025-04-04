a095cafe3068490271b01d865dfb03e88bb477cc
Dembolx <106410918+Dembolx@users.noreply.github.com>
2025-04-04 17:44:12 +0200

    dd

--- Space_Intruder/obj/Debug/net8.0-windows/App.g.i.cs
+++ Space_Intruder/obj/Debug/net8.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Space_Intruder {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace Space_Intruder {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Space_Intruder;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Space_Intruder;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace Space_Intruder {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public static void Main() {
             Space_Intruder.App app = new Space_Intruder.App();
             app.InitializeComponent();
